ef51d3b0670d8346ad0410e2e6397a07bdf5c98d
thien113 <n-tv@hotmail.de>
2023-05-06 21:11:03 +0200

    header basic styling

---
+++ components/Contact.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <button className="border border-orange-950 rounded-full col-start-6">
+      <a href="#">Contact Us</a>
+    </button>
+  );
+}
--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,17 @@
+import Contact from "./Contact";
+
 export default function Header() {
-  return <header class="font-bold">Header</header>;
+  return (
+    <header class="font-bold m-12 grid grid-cols-6">
+      <p>Logo</p>
+      <nav class="flex flex-row gap-4 col-span-3 col-start-3 font-normal">
+        <a href="#">Menu</a>
+        <a href="#">About Us</a>
+        <a href="#">Kinds of Coffee</a>
+        <a href="#">Blog</a>
+        <a href="#">Reviews</a>
+      </nav>
+      <Contact />
+    </header>
+  );
 }
