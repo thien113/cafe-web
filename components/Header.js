ec455c055732da6c54e94c0ec51be787f58f50c8
thien113 <n-tv@hotmail.de>
2023-05-08 17:36:10 +0200

    small bug fix

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,9 @@ import Contact from "./Contact";
 
 export default function Header() {
   return (
-    <header class="font-bold m-12 grid grid-cols-6">
+    <header className="font-bold m-12 grid grid-cols-6">
       <p>Logo</p>
-      <nav class="flex flex-row gap-4 col-span-3 col-start-3 font-normal">
+      <nav className="flex flex-row gap-4 col-span-3 col-start-3 font-normal">
         <a href="#">Menu</a>
         <a href="#">About Us</a>
         <a href="#">Kinds of Coffee</a>
