6608041df1266ab1e129008b415727f34d1efc4f
thien113 <n-tv@hotmail.de>
2023-05-13 17:25:19 +0200

    badges for our benefits

---
+++ components/Badge.js
@@ -0,0 +1,7 @@
+export default function Badge() {
+  return (
+    <div className="w-16 h-16 shrink-0 grow-0 rounded-full bg-coffee-accent flex justify-center items-center">
+      <div>hi</div>
+    </div>
+  );
+}
