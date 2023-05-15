acfcaaa0359a99ff8d8a2e598af71b80db3041b6
thien113 <n-tv@hotmail.de>
2023-05-15 13:10:08 +0200

    favorites import imgs

--- components/Favorites.js
+++ components/Favorites.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Button from "./Button";
 
 export default function Favorites() {
@@ -6,6 +7,14 @@ export default function Favorites() {
       <h4 className="text-4xl font-bold text-center text-coffee-dark underline underline-offset-8 decoration-coffee-accent">
         Favorites
       </h4>
+      <ul className="flex flex-col m-4">
+        <li className="flex flex-row">
+          <Image className="rounded-lg" src="/img/espresso.svg" width={100} height={100} alt="espresso"/>
+          <h3 className="text-2xl font-semibold mx-2">Espresso</h3> 
+          <div className="dotted"></div>
+          <p className="font-semibold">2.50€ </p>
+        </li>
+      </ul>
       <Button text="View Menu" />
     </section>
   );
