f8b47c6e7e6b3bb683c4d956aa56ffb91d131cc1
thien113 <n-tv@hotmail.de>
2023-05-16 11:57:43 +0200

    mobile header

--- pages/index.js
+++ pages/index.js
@@ -4,29 +4,25 @@ import Button from "@/components/Button";
 import Categories from "@/components/Categories";
 import Favorites from "@/components/Favorites";
 import Review from "@/components/Review";
-//import Image from "next/image";
+import Image from "next/image";
 
-/*const imageStyle = {
+const imageStyle = {
   backgroundSize: "cover",
   backgroundPosition: "center",
-};*/
+};
 export default function Home() {
   return (
     <>
-      <h1>
-        <span className="text-4xl font-bold">Enjoy Your Morning Coffee</span>
-      </h1>
-      <p className="text-md my-6 leading-none text-justify w-2/3 font-medium">
-        Boost your productivity and build your mood with a cup of coffee in the
-        morning.
-      </p>
-      <Button text="View Menu" />
-      {/* <Image
-        src="/coffee-cover.svg"
-        alt="coffee"
-        fill={true}
-        style={imageStyle}
-      />*/}
+      <section className="bg-no-repeat bg-contain bg-right bg-[url('/coffee-cover-1.svg')] h-auto">
+        <h1 className="text-4xl font-bold mt-12 pt-12">
+          Enjoy Your Morning Coffee
+        </h1>
+        <p className="text-md my-6 leading-none text-justify w-48 font-medium mb-12">
+          Boost your productivity and build your mood with a cup of coffee in the
+          morning.
+        </p>
+        <Button className="mt-24" text="View Menu" />
+      </section>
       <Categories />
       <Benefits  />
       <Favorites />
