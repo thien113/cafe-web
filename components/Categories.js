7a30498d17d5d7ace660ba98fdd2d1f004de36a2
thien113 <n-tv@hotmail.de>
2023-05-17 14:30:41 +0200

    header image is repsonsive

--- components/Categories.js
+++ components/Categories.js
@@ -1,6 +1,6 @@
 export default function Categories() {
   return (
-    <section className="p-8">
+    <section className="p-8 mt-12 md:mt-24">
       <h4 className="text-4xl font-bold text-center text-coffee-dark underline underline-offset-8 decoration-coffee-accent my-14">
         From Our Menu
       </h4>
--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,18 @@ const imageStyle = {
 export default function Home() {
   return (
     <>
-      <section className="bg-no-repeat bg-contain bg-right bg-[url('/coffee-cover-1.svg')] h-auto">
-        <h1 className="text-4xl font-bold mt-12 pt-12">
-          Enjoy Your Morning Coffee
+      <section className="my-4 bg-no-repeat bg-contain bg-right bg-[url('/coffee-cover-1.svg')] h-auto md:h-screen md:bg-right-top md:ml-12 lg:p-44">
+        <h1 className="text-4xl md:text-8xl font-bold mt-12 pt-12 ">
+          Enjoy Your <br/> Morning Coffee
         </h1>
-        <p className="text-md my-6 leading-none text-justify w-48 font-medium mb-12">
-          Boost your productivity and build your mood with a cup of coffee in the
+        <p className="text-md md:text-4xl my-12 lg:mt-48 md:mb-12 leading-none text-justify w-48 font-medium mb-12 md:w-full md:text-2xL">
+          Boost your productivity<br/> and build your mood with<br/> a cup of coffee in the
           morning.
         </p>
-        <Button className="mt-24" text="View Menu" />
+        
+        <div className=" md:w-1/2">
+          <Button  text="View Menu" /> 
+        </div>
       </section>
       <Categories />
       <Benefits  />
