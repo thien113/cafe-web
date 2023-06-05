09318d6474129cfc5e8218500f3ef189a54997b4
thien113 <n-tv@hotmail.de>
2023-06-05 17:37:15 +0200

    started working on review

--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,36 @@
-import Image from 'next/image';
+import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faBars
-} from "@fortawesome/free-solid-svg-icons";
-import logo from "../assets/brand/cozy-coffee.svg"
-import { useState } from 'react';
-import Link from 'next/link';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import logo from "../assets/brand/cozy-coffee.svg";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Header() {
   const [clicked, setClicked] = useState(false);
-  function toggle(){
+  function toggle() {
     setClicked(!clicked);
   }
   return (
-    <header className="flex flex-row justify-between items-start font-bold relative ">
+    <header className="flex flex-row justify-between items-start font-bold relative pl-4">
       <Link href="/">
-        <Image
-          src={logo}
-          width={180}
-          height={500}
-          alt="Cozy Coffee"
-        />
+        <Image src={logo} width={180} height={500} alt="Cozy Coffee" />
       </Link>
-      
-    <button className='md:hidden'><FontAwesomeIcon icon={faBars} style={{ fontSize: 25}} onClick={toggle} /></button>
-      {clicked && <nav className="flex flex-col gap-4 font-semibold absolute rounded-sm top-12 right-0 border p-8 bg-bg-light">
-        <a href="/menu">Menu</a>
-        <a href="/about">About Us</a>
-        <a href="/blog">Blog</a>
-        <a href="#">Reviews</a>
-      </nav>
-      }
+
+      <button className="md:hidden">
+        <FontAwesomeIcon
+          icon={faBars}
+          style={{ fontSize: 25 }}
+          onClick={toggle}
+        />
+      </button>
+      {clicked && (
+        <nav className="flex flex-col gap-4 font-semibold absolute rounded-sm top-12 right-0 border p-8 bg-bg-light">
+          <a href="/menu">Menu</a>
+          <a href="/about">About Us</a>
+          <a href="/blog">Blog</a>
+          <a href="#">Reviews</a>
+        </nav>
+      )}
       <nav className="hidden md:flex flex-row justify-between gap-8 font-semibold top-12 p-8 ">
         <a href="/menu">Menu</a>
         <a href="/about">About Us</a>
--- components/Review.js
+++ components/Review.js
@@ -4,15 +4,31 @@ import splash from "../assets/brand/splash.svg";
 
 export default function Review() {
   return (
-    <section className=" my-14 p-">
-      <h4 className="text-4xl font-bold text-center text-coffee-dark underline underline-offset-8 decoration-coffee-accent">
-        Loved By Many Customers
-      </h4>
-      <Image src={customer} sizes="(max-width: 200px) 10vw, (max-width: 200px) 20vw, 33vw" alt="customer"/>
-      <button className="rounded-full text-coffee-accent border-2 border-coffee-accent w-full p-2 font-semibold my-4">
-        View All
-      </button>
-      <Image src={splash} sizes="(max-width: 500px) 100vw, (max-width: 600px) 50vw, 33vw" alt="splash"/>
+    <section className=" mt-14 pt-8 flex flex-row justify-center items-center">
+      <Image
+        src={customer}
+        sizes="(max-width: 200px) 10vw, (max-width: 200px) 20vw, 33vw"
+        alt="customer"
+        className="-mr-48"
+      />
+      <div className="grow-2 w-full">
+        <h4 className="text-4xl font-bold text-center flex-1 text-coffee-dark underline underline-offset-8 decoration-coffee-accent">
+          Loved By Many Customers
+        </h4>
+        <p className="p-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+          officiis laboriosam quasi unde blanditiis aut nihil expedita quos,
+          veritatis, eaque, culpa dicta nostrum molestias! Dolore eius ipsum
+          similique consequatur ullam.
+        </p>
+      </div>
+
+      <Image
+        src={splash}
+        sizes="(max-width: 500px) 100vw, (max-width: 600px) 50vw, 33vw"
+        alt="splash"
+        className="ml-48"
+      />
     </section>
   );
 }
