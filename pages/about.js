ddcd2a9239c7a3bce498b887c0c3136eb6f23c9d
thien113 <n-tv@hotmail.de>
2023-06-12 17:51:28 +0200

    add favicon

--- pages/about.js
+++ pages/about.js
@@ -18,11 +18,11 @@ export default function About(){
           alt="about-us"/>
           <div className="m-4 lg:m-12 block">
             <i>
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+            &quot;Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
           exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure<br/>
           dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.&quot;
           </i>
           <ul className="m-4 lg:m-12 flex flex-col ">
             <p className="font-semibold">Contact</p>
